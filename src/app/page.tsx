d903747019137a99053d15d746cf74e4a3a40d6d
Angel Jair Neri Huerta <yayodcv@gmail.com>
2023-09-20 16:16:03 -0600

    state with primitive type

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,25 @@
+'use client';
 import { RandomFox } from '@components/RandomFox';
 import Image from 'next/image';
+import { useState } from 'react';
 
 export default function Home() {
 	const randomNumber = () => Math.floor(Math.random() * 123) + 1;
+	const [images, setImages] = useState<Array<string>>([
+		`https://randomfox.ca/images/${randomNumber()}.jpg`,
+		`https://randomfox.ca/images/${randomNumber()}.jpg`,
+		`https://randomfox.ca/images/${randomNumber()}.jpg`,
+	]);
+
 	const image = `https://randomfox.ca/images/${randomNumber()}.jpg`;
 
 	return (
 		<main>
-			<RandomFox image={image} />
+			{images.map((image, index) => (
+				<div className="p-4" key={index}>
+					<RandomFox image={image} />
+				</div>
+			))}
 		</main>
 	);
 }
