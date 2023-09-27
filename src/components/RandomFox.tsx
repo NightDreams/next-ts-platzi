07e88809e889217314ccfaf16beed5427ea59414
Angel Jair Neri Huerta <yayodcv@gmail.com>
2023-09-26 22:03:21 -0600

    Typing and use a node ref

--- src/components/RandomFox.tsx
+++ src/components/RandomFox.tsx
@@ -1,10 +1,33 @@
+import { useEffect, useRef } from 'react';
+
 type Props = {
 	image: string;
 };
 
 export const RandomFox = ({ image }: Props): JSX.Element => {
+	const node = useRef<HTMLImageElement>(null);
+
+	useEffect(() => {
+		const observer = new IntersectionObserver(entries => {
+			entries.forEach(entry => {
+				if (entry.isIntersecting) {
+					console.log('Intersecting');
+				}
+			});
+		});
+
+		if (node.current) {
+			observer.observe(node.current);
+		}
+
+		return () => {
+			observer.disconnect;
+		};
+	}, []);
+
 	return (
 		<img
+			ref={node}
 			width="320"
 			height="auto"
 			src={image}
